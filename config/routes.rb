297324302581751a277089c044a07772bc8af26d
RubyOnRails Udemy Course <lightoecho@gmail.com>
2016-08-17 13:24:29 -0400

    create new template, action and route

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,15 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
 
-
+  resources :articles
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'pages#index'
 
-  get 'pages/home', to: 'pages#home'
-  get 'pages/about', to: 'pages#about'
+  #get 'pages/home', to: 'pages#home'
+  #get 'pages/about', to: 'pages#about'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
